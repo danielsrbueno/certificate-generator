3bea4ad1503ae3b140da55286d7126ecf7aecdcf
Daniel Bueno <111004405+danielsrbueno@users.noreply.github.com>
2026-06-16 20:09:47 -0300

    feat: add primary button component

---
+++ src/app/components/primary-button/primary-button.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-button',
+  imports: [],
+  templateUrl: './primary-button.html',
+  styleUrl: './primary-button.css',
+})
+export class PrimaryButton {
+  @Input() buttonText: string = ""
+  @Input() disabled: boolean = false
+}
