760968486a9f3b2c73eccffb42e343571c64c997
Daniel Bueno <111004405+danielsrbueno@users.noreply.github.com>
2026-06-16 20:13:38 -0300

    feat: add local storage

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./components/navbar/navbar";
+import { BaseUi } from './components/base-ui/base-ui';
+import { Certificate } from './services/certificate';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, BaseUi],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('gerador-certificado');
+  exibeNavbar: boolean = true
+
+  constructor (private certificateService: Certificate) {}
+
+  ngOnInit(): void {
+    const storageCertficates = localStorage.getItem('certificates')
+    this.certificateService.certificates = storageCertficates ? JSON.parse(storageCertficates) : []
+  }
 }
